a4b017e124773afd5f0ed76e3fa7104290c2a474
Anurag Arwalkar <anurag.arwalkar@globant.com>
2024-05-21 00:06:09 +0530

    first commit

---
+++ Contents.swift
@@ -0,0 +1,31 @@
+// Variable declaration
+var useName = "Hitesh";
+var numberValue = 1.2;
+
+let newNumber = 122;
+
+var numberOfLogin:Int = 13;
+
+
+
+// Range in swift
+var result = 1 + 2;
+
+var myRange = 1..<5;
+
+//print(myRange.contains(5));
+
+
+// String
+
+var greetings = "Hello Playground";
+var userName:String = "asd";
+
+var age = 44;
+
+var myFinalNotice = "User's age is \(age) and he lives in canada";
+
+print(myFinalNotice.split(separator: " "));
+
+
+
